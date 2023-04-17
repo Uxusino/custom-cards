c18bed80ca59d6892999d54ad662c433fda43ece
Uxusino Uxusone <89293277+Uxusino@users.noreply.github.com>
2023-04-17 15:18:27 +0300

    Minor style updates

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
+
+CORS(app)
 
 languages = []
 with open("languages.txt", "r") as file:
